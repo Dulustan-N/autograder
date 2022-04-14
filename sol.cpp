d38fabaf59b398b03fb16dabcf714c54bf53f908
Dulustan <18385763+Dulustan-N@users.noreply.github.com>
2022-04-14 13:58:18 +0900

    second

--- sol.cpp
+++ sol.cpp
@@ -1,23 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int solve(int a, int b)
-{
-    //Пишите ваш код тут
-    return a+b;
-}
-
-#ifdef TESTING
-#define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( solve(1, 1) == 2 );
-    REQUIRE( solve(2'147'483'648, 2'147'483'647) == -1 );
-}
-#else
 int main()
 {
-    int a, b; cin >> a >> b;
-    cout << solve(a, b) << endl;
+    
 }
-#endif
